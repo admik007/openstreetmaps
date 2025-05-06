379dd2a84c7363e57684bb838bca647e7be27668
Tomas KOVAC <office@ztk-comp.sk>
2025-05-06 15:34:06 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,21 +13,68 @@ if [ ! -f /var/lib/postgresql/11/main/PG_VERSION_ok ]; then
  touch /var/lib/postgresql/11/main/PG_VERSION_ok
 fi
 
+if [ ! -f /var/www/html/AddressSearch.css ]; then
+ cp /var/www/html_bak/AddressSearch.css /var/www/html/AddressSearch.css
+fi
+
+
+if [ ! -f /var/www/html/AddressSearch.js ]; then
+ cp /var/www/html_bak/AddressSearch.js /var/www/html/AddressSearch.js
+fi
+
+
+if [ ! -f /var/www/html/favicon.ico ]; then
+ cp /var/www/html_bak/favicon.ico /var/www/html/favicon.ico
+fi
+
+
 if [ ! -f /var/www/html/index.php ]; then
  cp /var/www/html_bak/index.php /var/www/html/index.php
 fi
+
 
 if [ ! -f /var/www/html/leaflet.css ]; then
  cp /var/www/html_bak/leaflet.css /var/www/html/leaflet.css
 fi
 
+
 if [ ! -f /var/www/html/leaflet.js ]; then
  cp /var/www/html_bak/leaflet.js /var/www/html/leaflet.js
 fi
 
+
+if [ ! -f /var/www/html/leaflet.routing.icons.png ]; then
+ cp /var/www/html_bak/leaflet.routing.icons.png /var/www/html/leaflet.routing.icons.png
+fi
+
+
+if [ ! -f /var/www/html/leaflet-routing-machine.css ]; then
+ cp /var/www/html_bak/leaflet-routing-machine.css /var/www/html/leaflet-routing-machine.css
+fi
+
+
+if [ ! -f /var/www/html/leaflet-routing-machine.js ]; then
+ cp /var/www/html_bak/leaflet-routing-machine.js /var/www/html/leaflet-routing-machine.js
+fi
+
+
+if [ ! -f /var/www/html/marker-icon.png ]; then
+ cp /var/www/html_bak/marker-icon.png /var/www/html/marker-icon.png
+fi
+
+
+if [ ! -f /var/www/html/ro_dialnice.php ]; then
+ cp /var/www/html_bak/ro_dialnice.php /var/www/html/ro_dialnice.php
+fi
+
+
+if [ ! -f /var/www/html/routing-icon.png ]; then
+ cp /var/www/html_bak/routing-icon.png /var/www/html/routing-icon.png
+fi
+
 if [ ! -d /var/lib/mod_tile/default ]; then
- mkdir -p /var/lib/mod_tile/default/
- chmod 777 /var/lib/mod_tile/default/
+ mkdir -p /var/lib/mod_tile/default
+ chmod 777 /var/lib/mod_tile/default
 fi
 
 /etc/init.d/postgresql restart
